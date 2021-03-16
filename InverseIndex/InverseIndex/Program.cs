cb4989c07c92dfa7391b46011f957e483544b8f2
Yuniya Kim <kim.yuniya@mail.ru>
2021-03-16 13:48:21 +0300

    Created project

---
+++ InverseIndex/InverseIndex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InverseIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
