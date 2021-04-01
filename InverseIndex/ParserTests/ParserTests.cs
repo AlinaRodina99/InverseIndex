8f71b5dc71b6329e1b45cbe68835d115be6c1756
Yuniya Kim <kim.yuniya@mail.ru>
2021-04-01 13:42:35 +0300

    Added more tests

--- InverseIndex/ParserTests/ParserTests.cs
+++ InverseIndex/ParserTests/ParserTests.cs
@@ -119,5 +119,19 @@ namespace ParserTests
             parser = new Parser("NOT OR word1 OR word2 AND word3");
             Assert.Throws<ArgumentException>(() => parser.Parse());
         }
+
+        [Test]
+        public void YetAnotherInvalidInputParseTest()
+        {
+            parser = new Parser("NOT word1 OR AND word2 word3");
+            Assert.Throws<ArgumentException>(() => parser.Parse());
+        }
+
+        [Test]
+        public void YetOneMoreInvalidInputParseTest()
+        {
+            parser = new Parser("NOT word1 word2 AND OR word3");
+            Assert.Throws<ArgumentException>(() => parser.Parse());
+        }
     }
 }
