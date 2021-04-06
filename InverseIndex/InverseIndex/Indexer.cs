e4aecdbf9d54495ec40e9f0d21877f10166907d9
Yuniya Kim <kim.yuniya@mail.ru>
2021-04-06 20:57:12 +0300

    Added processor's output

--- InverseIndex/InverseIndex/Indexer.cs
+++ InverseIndex/InverseIndex/Indexer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace InverseIndex
 {
@@ -65,8 +66,8 @@ namespace InverseIndex
                 Console.WriteLine("Incorrect query.");
             }
 
-            var processor = new Processor(pathToIndex);
-            processor.Process(parsedInput);
+            var processor = new Processor(pathToIndex, Enumerable.Range(0, Directory.GetFiles(pathToCorpus).Length).ToArray());
+            Console.WriteLine($"Documents' ids: {processor.Process(parsedInput)}");
         }
 
         /// <summary>
