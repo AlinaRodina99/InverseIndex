1abf6e5c3ae23a634a03b0963a39c412a39e14dd
Yuniya Kim <kim.yuniya@mail.ru>
2021-04-16 03:02:00 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 dotnet build InverseIndex/InverseIndex.sln
 dotnet test InverseIndex/InverseIndex.sln
 dotnet InverseIndex/InverseIndex/bin/Debug/netcoreapp3.1/InverseIndex.dll
-read -p "Press any key to exit..."
+read -n 1 -s
